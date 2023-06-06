f007f391c18cecab786241d967208b8260f7dad2
Ismail Hussein <133203884+Ismail2hussein@users.noreply.github.com>
2023-06-06 10:26:23 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+const ages = [[34,23,12],[78,23,8],[12]]
+
+for (let i=0;i<ages.length;i++){
+for (let j=0;j<ages[i].length;j++){
+   if (ages[i][j] > 20 && ages[i][j] < 40){
+console.log(ages[i][j])
+   }
+
+}
+}
